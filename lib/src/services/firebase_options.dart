2db9ed8700ae610728be05f12d16f2d7c5986780
suysoftware <suy.register@gmail.com>
2022-11-02 13:00:19 +0300

    codes

---
+++ lib/src/services/firebase_options.dart
@@ -0,0 +1,15 @@
+import 'package:firebase_core/firebase_core.dart';
+
+ class FirebaseOptionsClass{
+  static const FirebaseOptions firebaseConfig = FirebaseOptions(
+    apiKey: "AIzaSyD01UewrwgXUQOqB8xZhc3xZ_HqoERMafw",
+    authDomain: "",
+    projectId: "dbripper-b935d",
+    storageBucket: "",
+    messagingSenderId: "",
+    appId: "1:198762541744:ios:0db92032bf56b994100175",
+    measurementId: "");
+
+
+
+    }
